f273c1f0d44c1a483756db9570fff85155613191
eklochkova101 <260723543+eklochkova101@users.noreply.github.com>
2026-04-19 21:55:06 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,24 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2026 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+	T data[size];
+	int top;
+public:
+	TStack() : top(-1) {}
+	void push(T value) { if (top < size - 1) data[++top] = value; }
+	T pop() {
+    if (!isEmpty()) { return data[top--]; }
+		return T();
+	}
+	bool isEmpty() { return top == -1; }
+	T getTop() {
+		if (!isEmpty()) return data[top];
+		return T();
+	}
 };
 
 #endif  // INCLUDE_TSTACK_H_
